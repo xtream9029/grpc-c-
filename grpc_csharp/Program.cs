395fd75232271c99a6eec879fc3f9a0546707535
이성원 <neo_xtream@iseong-won-ui-MacBookPro.local>
2024-10-25 18:13:37 +0900

    테스트 프로젝트 초기

---
+++ grpc_csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Helloworld; // 프로토에서 생성된 네임스페이스
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // gRPC 서버에 연결
+        using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+        var client = new Greeter.GreeterClient(channel);
+
+        // 서버에 Hello 요청
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "C# Client" });
+
+        // 응답 출력
+        Console.WriteLine("Greeting: " + reply.Message);
+    }
+}
+
---
+++ grpc_go/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "context"
+    "log"
+    "net"
+
+    "google.golang.org/grpc"
+    pb "helloworld/proto_src" // proto 파일에서 생성된 패키지
+)
+
+type server struct {
+    pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    pb.RegisterGreeterServer(s, &server{})
+    log.Printf("server listening at %v", lis.Addr())
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
+
